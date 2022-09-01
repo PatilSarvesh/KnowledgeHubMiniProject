2d1788c45d2bd8fcb3af9ad147f94db0c936effa
PSarvesh <PSarvesh@OPT-LAP-0651>
2022-09-01 21:57:51 +0530

    Add project files.

---
+++ KnowledgeHub.Web/Controllers/CatagoriesController.cs
@@ -0,0 +1,80 @@
+﻿using KnowledgeHub.Web.Models.Data;
+using KnowledgeHub.Web.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace KnowledgeHub.Web.Controllers
+{
+    public class CatagoriesController : Controller
+    {
+        KnowledgeHubDbContext db = new KnowledgeHubDbContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //for  create
+        //Catagories/create
+       
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Save(Catagory catagory)
+        {
+            
+            //do Validation   
+            if(!ModelState.IsValid)
+                return View("Create");
+ 
+            db.Add(catagory);
+            db.SaveChanges();
+            TempData["Message"] = $"Catagory {catagory.Name} Has created";
+            
+            //return View("Display", db.Catagories.ToList());
+            return RedirectToAction("Display");
+        }
+
+        public IActionResult Display()
+        {
+
+            //DataSet data = new DataSet();
+            //var result = from c in db.Catagories
+            //             select c;
+
+
+            List<Catagory> catagories = db.Catagories.ToList();
+            //ViewBag.CatagoryList = from c  in db.Catagories
+            //                       select c;
+
+            return View(catagories);
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var res = db.Catagories.Find(id);
+            return View(res);
+        }
+        public IActionResult Del(int id)
+        {
+            if (!ModelState.IsValid)
+                return View("Create");
+
+            var res = db.Catagories.Find(id);
+            db.Catagories.Remove(res);
+            db.SaveChanges();
+
+            return RedirectToAction("Display");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var res = db.Catagories.Find(id);
+            return View(res);
+        }
+    }
+}
---
+++ KnowledgeHub.Web/Models/Entities/Catagory.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KnowledgeHub.Web.Models.Entities
+{
+    public class Catagory
+    {
+        public int CatagoryId { get; set; }
+        [Required,MaxLength(50)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
